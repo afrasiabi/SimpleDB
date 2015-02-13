5a5c7300b687ecc1a59e2ce127e299e4b1d9ba85
azadeh afrasiabi <azadehafrasiabi@outlook.com>
2015-02-13 15:05:13 +0330

    Changed startup from client/server style to single app

--- simpledb/server/Startup.java
+++ simpledb/server/Startup.java
@@ -1,20 +1,43 @@
 package simpledb.server;
 
-import simpledb.remote.*;
-import java.rmi.registry.*;
+
+import simpledb.parse.Parser;
+import simpledb.query.Plan;
+import simpledb.query.Scan;
+import simpledb.tx.Transaction;
 
 public class Startup {
    public static void main(String args[]) throws Exception {
-      // configure and initialize the database
-      SimpleDB.init(args[0]);
+     try {
+         // analogous to the driver
+      SimpleDB.init("studentdb");
       
-      // create a registry specific for the server on the default port
-      Registry reg = LocateRegistry.createRegistry(1099);
+         // analogous to the connection
+      Transaction tx = new Transaction();
       
-      // and post the server entry in it
-      RemoteDriver d = new RemoteDriverImpl();
-      reg.rebind("simpledb", d);
+         // analogous to the statement
+      String qry = "select SName, DName "
+      + "from DEPT, STUDENT "
+      + "where MajorId = DId"; 
+      Plan p = SimpleDB.planner().createQueryPlan(qry, tx);
       
-      System.out.println("database server ready");
+         // analogous to the result set
+      Scan s = p.open();
+      
+      System.out.println("Name\tMajor");
+      while (s.next()) {
+            String sname = s.getString("sname"); //SimpleDB stores field names
+            String dname = s.getString("dname"); //in lower case
+            System.out.println(sname + "\t" + dname);
+         }
+         s.close();
+         tx.commit();
+
+
+      }
+      catch(Exception e) {
+      }
+
    }
 }
+
